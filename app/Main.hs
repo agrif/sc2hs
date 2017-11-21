b3f18e147528626705c59b243724bb397e439e8d
Aaron Griffith <aargri@gmail.com>
2017-11-21 13:49:40 -0500

    moved main to the actual main file

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,19 @@ module Main where
 import Network.SC2
 
 main :: IO ()
-main = someFunc
+main = withMain mempty (runLocal (runSC2 bot))
+
+bot :: SC2M ()
+bot = do
+  Right info <- syncRequest Ping
+  send (putStrLn $ "info: " ++ show info)
+
+  Right maps@(map : _) <- syncRequest AvailableMaps
+  send (putStrLn $ "maps: " ++ show maps)
+
+  r <- syncRequest $ CreateGame map [Participant Protoss, Computer Terran Medium]
+  send (putStrLn $ "create game: " ++ show r)
+
+  r <- syncRequest $ JoinGame Protoss [Raw]
+  send (putStrLn $ "join game: " ++ show r)
+
--- src/Network/SC2.hs
+++ src/Network/SC2.hs
@@ -1,23 +1,11 @@
-module Network.SC2 (someFunc) where
+module Network.SC2
+  ( module Network.SC2.Mainable
+  , module Network.SC2.Process
+  , module Network.SC2.Protocol
+  , module Network.SC2.Requests
+  ) where
 
 import Network.SC2.Mainable
 import Network.SC2.Process
 import Network.SC2.Protocol
 import Network.SC2.Requests
-
-someFunc :: IO ()
-someFunc = withMain mempty (runRemote (runSC2 bot))
-
-bot :: SC2M ()
-bot = do
-  Right info <- syncRequest Ping
-  send (putStrLn $ "info: " ++ show info)
-
-  Right maps <- syncRequest AvailableMaps
-  send (putStrLn $ "maps: " ++ show maps)
-
-  r <- syncRequest $ CreateGame (LocalMap "/opt/StarCraftII/Maps/Simple64.SC2Map" Nothing) [Participant Protoss, Computer Terran Medium]
-  send (putStrLn $ "create game: " ++ show r)
-
-  r <- syncRequest $ JoinGame Protoss [Raw]
-  send (putStrLn $ "join game: " ++ show r)
